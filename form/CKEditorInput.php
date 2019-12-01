e4cb8379ae9863b354a75d375f01b5a2e9b91c77
goph-R <gopher.hu@gmail.com>
2019-12-02 00:34:43 +0100

    Initial commit.

---
+++ form/CKEditorInput.php
@@ -0,0 +1,41 @@
+<?php
+
+class CKEditorInput extends Input {
+    
+    private $baseUrl;
+    private $options = [];
+    
+    public function __construct(Framework $framework, $name, $defaultValue = '') {
+        parent::__construct($framework, $name, $defaultValue);
+        /** @var Router $router */
+        $router = $framework->get('router');
+        $this->baseUrl = $router->getBaseUrl();
+    }
+    
+    public function setOptions($options) {
+        $this->options = $options;
+    }
+
+    public function fetch() {        
+        $result = '<textarea ';
+        $result .= ' id="'.$this->getId().'"';
+        $result .= ' name="'.$this->form->getName().'['.$this->getName().']"';
+        $result .= $this->getClassHtml();
+        $result .= '>'.$this->getValue().'</textarea>';
+        $this->view->addScript($this->baseUrl.'modules/minicore-ckeditor/static/ckeditor.js');
+        $this->view->appendBlock('scripts');
+        $optionsJson = json_encode($this->options);
+        $this->view->write('
+            <script type="text/javascript">
+                ClassicEditor
+                    .create( document.querySelector( \'#'.$this->getId().'\' ), '.$optionsJson.' )
+                    .catch( error => {
+                        console.error( error );
+                    } );
+            </script>
+        ');
+        $this->view->endBlock();
+        return $result;
+    }
+
+}
